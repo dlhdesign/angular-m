bab0cdfca2a19a8eb9c23b11e3f22f90feeebff4
David Hutchings <dhutchings@vivoinspire.com>
2015-08-06 23:20:25 -0700

    release 0.1.3

--- release/angular-m.min.js
+++ release/angular-m.min.js
@@ -1,6 +1,6 @@
 /**
  * Angular-based model library for use in MVC framework design
- * @version v0.1.2
+ * @version v0.1.3
  * @link https://github.com/dlhdesign/angular-m
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
