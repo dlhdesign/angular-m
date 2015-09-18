fe4b9225ef82f0ab9edd80bbc7b638bee4c3370a
David Hutchings <dhutchings@vivoinspire.com>
2015-09-18 16:17:02 -0700

    pre-release commit for 1.0.2

--- angular-m.js
+++ angular-m.js
@@ -1,6 +1,6 @@
 /**
  * Angular-based model library for use in MVC framework design
- * @version v1.0.1
+ * @version v1.0.2
  * @link https://github.com/dlhdesign/angular-m
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
